50f52627648ed9ec7523ca0d658d44caa8a4d841
Lucas Godoy <lhskatista@gmail.com>
2023-03-12 12:37:19 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import "./styles/main.scss";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+]);
+
 const App = () => {
-  return (
-    <div>
-      <LandingPage />
-    </div>
-  );
+  <RouterProvider router={router} />;
 };
 
 export default App;
